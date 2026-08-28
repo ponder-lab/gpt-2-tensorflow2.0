029f6bfd33025e1d6acf31f92dd600ca16e24aff
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2026-08-28 10:10:30 -0400

    Regenerate hybridize-pristine under Ariadne 0.52.87
    Tool output of the hybridization refactoring on master with performChange on and
    inference false: the honest unsignatured (none) arm, with no hand-tuned decorator arguments.

--- data_pipeline.py
+++ data_pipeline.py
@@ -32,6 +32,7 @@ def convert_ids_to_tokens(inv_vocab, ids):
     return convert_by_vocab(inv_vocab, ids)
 
 
+@tf.function
 def parse_example(serialized_example):
     data_fields = {
         "inputs": tf.io.VarLenFeature(tf.int64),
@@ -47,6 +48,7 @@ def parse_example(serialized_example):
     return inputs, targets
 
 
+@tf.function
 def input_fn(tf_records,
              batch_size=32,
              padded_shapes=([-1], [-1]),
